17fa49a554fbd37b6bbeca4538a51682b55232fd
Jack Peterson <jlpeterson829@gmail.com>
2018-11-13 15:14:15 -0700

    Remove unneeded observable

--- src/screens/TodoList.js
+++ src/screens/TodoList.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
-import { observer } from 'mobx-react'
 
 import TodoHeader from '../components/TodoHeader'
 import TodoItem from '../components/TodoItem'
 import TodoFooter from '../components/TodoFooter'
 
-@observer
 class TodoList extends Component {
-
-  onPressItem = (index) => {
-    const { todos } = this.props.store
-    console.log('todos ', todos)
-    todos[index].completed = !todos[index].completed
-  }
-
   render() {
     return (
       <FlatList
@@ -22,7 +13,7 @@ class TodoList extends Component {
         keyExtractor={({ id }) => `${id}`}
         ListHeaderComponent={TodoHeader}
         ListFooterComponent={() => <TodoFooter {...this.props} />}
-        renderItem={(item) => <TodoItem {...item} index={item.index} pressHandler={this.onPressItem} />}
+        renderItem={(item) => <TodoItem {...item} />}
       />
     );
   }
